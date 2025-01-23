9d134de09e054f5dfceedc5ae4cb7b3d2a3917ee
Michael Victorino <michaelvictorino@gmail.com>
2025-01-22 22:30:18 -0500

    First commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <SDL2/SDL.h>
+
+int main()
+{
+        //init everything within sdl2
+        SDL_Init( SDL_INIT_EVERYTHING );
+        
+        //create window
+        SDL_Window* window = SDL_CreateWindow(
+        "Title",                //title 
+        300,                    //x position
+        300,                    //y position
+        1280,                   //width
+        720,                    //height
+        SDL_WINDOW_SHOWN);      //flags
+
+        if(window == nullptr)
+        {
+                SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+                return 1;
+        }
+
+        //delay for a bit
+        SDL_Delay(3000);
+
+        SDL_DestroyWindow(window);
+
+        SDL_Quit();
+
+        return 0;
+}
